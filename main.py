5a9441a0768064a9714a2aac3b0861b1b1753d60
Ayden M. Newcomb <aydennewcomb1@gmail.com>
2026-05-20 20:14:16 -0700

    Added the clicker building and added a delay between cookie taps (that does not work very well). Also added the log command which lists all of the owned buildings.

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from time import sleep
 
-testing = True
+testing: bool = False
 
 cookies: int = 0
-buildings_owned: dict[str, int] = {"Grandma": 0, "Placeholder": 0}
+buildings_owned: dict[str, int] = {"Clicker": 0, "Grandma": 0}
+click_delay: float = 0.5
+
+def openLog(buildings_owned: dict[str, int]):
+    print()
+    for key, value in buildings_owned.items():
+        print(f"{key}: {value}")
+        sleep(0.5)
 
 def cookieTap(buildings_owned: dict[str, int], click: int, mult: float):
     building_amount: int = 0
     for key, value in buildings_owned.items():
-        print(key, value)
-        building_amount += value 
+        match key:
+            case "Clicker":
+                building_amount += value * 1
+            case "Grandma":
+                building_amount += value * 5
+            case _:
+                pass
 
+    sleep(click_delay)
     return int((click + building_amount) * mult)
 
-def openShop(buildings: dict[str, int], cookies):
+def openShop(buildings: dict[str, int], cookies: int):
     print("Shop:")
-    print("1. Grandma -> 100 cookies")
-    print("2. Placeholder -> 1 cookies")
+    print("1. Clicker -> 15 cookies")
+    print("2. Grandma -> 100 cookies")
 
     checkout = 0
 
@@ -24,14 +37,19 @@ def openShop(buildings: dict[str, int], cookies):
     
     match user_selection:
         case "1":
+            if cookies >= 15:
+                print(f"{buildings["Clicker"]} -> {buildings["Clicker"] + 1}")
+                buildings["Clicker"] += 1
+                checkout -= 15
+            else:
+                print("Not enough cookies...")
+        case "2":
             if cookies >= 100:
                 print(f"{buildings["Grandma"]} -> {buildings["Grandma"] + 1}")
                 buildings["Grandma"] += 1
                 checkout -= 100
             else:
                 print("Not enough cookies...")
-        case "2":
-            print(buildings["Grandma"])
         case _:
             print("Invalid command...")
 
@@ -66,12 +84,14 @@ def main():
             case "":
                 cookies += cookieTap(buildings_owned, 1, 1)
             case "shop":
-                print("Shop executed")
+                print()
                 cookies += openShop(buildings_owned, cookies)
             case "quit":
+                print("\nGoodbye!")
+                sleep(click_delay)
                 exit()
-            case "list":
-                print("List executed")
+            case "log":
+                openLog(buildings_owned)
             case _:
                 print("???")
 
