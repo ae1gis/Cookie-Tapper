60bd634d72a694bf30ccfd8bf7bd621a46751ba4
Ayden M. Newcomb <aydennewcomb1@gmail.com>
2026-05-20 19:46:47 -0700

    You can now purchase grandmas to increase the amount of cookies you get per tap. Also added README.md because it is good practice.

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple recreation of the popular video game Cookie Clicker
+in python. This project is simply for me to get better at Python,
+Git, and GitHub.
--- main.py
+++ main.py
@@ -5,26 +5,41 @@ testing = True
 cookies: int = 0
 buildings_owned: dict[str, int] = {"Grandma": 0, "Placeholder": 0}
 
-def cookieTap(buildings: int, click: int, mult: float):
-    return int((click + buildings) * mult)
+def cookieTap(buildings_owned: dict[str, int], click: int, mult: float):
+    building_amount: int = 0
+    for key, value in buildings_owned.items():
+        print(key, value)
+        building_amount += value 
 
-def openShop(buildings: dict[str, int]):
+    return int((click + building_amount) * mult)
+
+def openShop(buildings: dict[str, int], cookies):
     print("Shop:")
     print("1. Grandma -> 100 cookies")
     print("2. Placeholder -> 1 cookies")
+
+    checkout = 0
 
     user_selection: str = input("Enter a number >>> ")
     
     match user_selection:
         case "1":
-            print(f"{buildings["Grandma"]} -> {buildings["Grandma"] + 1}")
+            if cookies >= 100:
+                print(f"{buildings["Grandma"]} -> {buildings["Grandma"] + 1}")
+                buildings["Grandma"] += 1
+                checkout -= 100
+            else:
+                print("Not enough cookies...")
         case "2":
             print(buildings["Grandma"])
         case _:
-            pass
+            print("Invalid command...")
+
+    return checkout
 
 def main():
     global cookies
+    global buildings_owned
 
     if not testing:
         print("Welcome to Cookie Tapper!\n")
@@ -49,10 +64,10 @@ def main():
 
         match user_input:
             case "":
-                cookies += cookieTap(0, 1, 1)
+                cookies += cookieTap(buildings_owned, 1, 1)
             case "shop":
                 print("Shop executed")
-                openShop(buildings_owned)
+                cookies += openShop(buildings_owned, cookies)
             case "quit":
                 exit()
             case "list":
